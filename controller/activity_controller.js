66415ddc6ab0cf76d8e46503bf0593a4f14b9610
phumipatapi <programmer-1@Macintosh.local>
2023-03-30 05:49:08 +0700

    back to normal

--- controller/activity_controller.js
+++ controller/activity_controller.js
@@ -6,62 +6,16 @@ const {
     updateActivityById,
     updateStatusActivityById,
 } = require('../model/activity_model')
-const { v4: uuidv4 } = require('uuid')
-const fs = require('fs')
+
 
 
 
 module.exports = {
     insertActivityController: async (req, res, next) => {
         try {
-            const {
-                district,
-                activity_type,
-                is_use_to_activity,
-                activity_name,
-                activity_detail,
-                activity_time,
-                address,
-                latitude,
-                longtitude,
-                participation_limit,
-                activity_price,
-                status,
-                user_id,
-            } = req.body
-
-            const activity_images = []
-
-            // Loop through each image and save it to a local directory
-            for (let i = 0; i < req.body.activity_image.length; i++) {
-                const file = req.body.activity_image[i]
-                const extension = file.split(';')[0].split('/')[1]
-                const fileName = `${uuidv4()}.${extension}`
-                const imagePath = `${__dirname}/../public/images/${fileName}`
-                const base64Data = file.replace(/^data:image\/\w+;base64,/, '')
-                fs.writeFileSync(imagePath, base64Data, { encoding: 'base64' })
-                activity_images.push(`http://${req.headers.host}/images/${fileName}`) // Save the URL to the image in the activity_images array
-            }
-
-            const activityData = {
-                district,
-                activity_type,
-                is_use_to_activity,
-                activity_name,
-                activity_detail,
-                activity_time,
-                address,
-                latitude,
-                longtitude,
-                activity_images, // Save the array of image URLs in the activity_images field
-                participation_limit,
-                activity_price,
-                status,
-                user_id,
-            }
-
-            const response = await insertActivityQuery(activityData)
-            res.status(200).json(response)
+            const jsonResponse = await insertActivity(req.body)
+            res.status(200)
+            res.json(jsonResponse)
         } catch (err) {
             next(err)
         }
